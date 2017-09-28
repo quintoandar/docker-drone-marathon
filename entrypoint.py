5d21fd759844ec34184022296b332ad0b3e4973b
Fernando Barbosa <fernando.barbosa@quintoandar.com.br>
2017-09-28 18:13:12 -0300

    Replace try/except by an if check of sys.args

--- entrypoint.py
+++ entrypoint.py
@@ -7,29 +7,40 @@ import json
 import requests
 
 
-def replace_secrets(d):
-    for k, v in d.iteritems():
-        if isinstance(v, basestring) and v[0:2] == "${" and v[-1] == "}":
-            d[k] = os.getenv(re.sub('[\$\{\}]', '', v))
-    return d
+def replace_secrets(env):
+    for key, val in env.iteritems():
+        if isinstance(val, basestring) and val[0:2] == "${" and val[-1] == "}":
+            env[key] = os.getenv(re.sub(r'[\$\{\}]', '', val))
+    return env
 
 
-if __name__ == "__main__":
-    try:
-        # try legacy config (drone 0.4)
+def main():
+
+    if len(sys.argv) > 2:
         config = json.loads(sys.argv[2]).get("vargs")
         server = config.get("server")
         app = config.get("app_config")
         print "Running legacy version (Drone 0.4)"
-    except IndexError:
-        # else try drone 0.5+ config
+    else:
         server = os.getenv("PLUGIN_SERVER")
         app = json.loads(os.getenv("PLUGIN_APP_CONFIG"))
+
+    if not app:
+        raise RuntimeError("Missing 'app_config' parameter")
+    if not server:
+        raise RuntimeError("Missing 'server' parameter")
 
     print json.dumps(app, indent=4)
 
     if "env" in app:
         app['env'] = replace_secrets(app["env"])
 
-    r = requests.put('%s/v2/apps/%s' % (server, app.get("id")), json=app)
-    print r.text
+    resp = requests.put(
+        '%s/v2/apps/%s?force=true' % (server, app.get("id")), json=app)
+
+    print resp.text
+    resp.raise_for_status()
+
+
+if __name__ == "__main__":
+    main()
